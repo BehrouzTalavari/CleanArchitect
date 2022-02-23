84a7e6cc0ae9e497999df1558eeae7f04cc33210
Behrouz Talavari <b.talavari@persici.co.uk>
2022-02-23 21:08:05 +0330

    Add Interceptor

--- Business/DependancyResolver/Autofac/AutofactBusinessModule.cs
+++ Business/DependancyResolver/Autofac/AutofactBusinessModule.cs
@@ -4,21 +4,31 @@ using Business.Concrete;
 using Core.Utility.Security.Jwt;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
+using Autofac.Extras.DynamicProxy;
+using Core.Utility.Interceptors;
 
 namespace Business.DependancyResolver.Autofac
 {
-    public class AutofactBusinessModule:Module
+    public class AutofactBusinessModule : Module
     {
         protected override void Load(ContainerBuilder builder)
         {
-            builder.RegisterType<ProductManager>().As<IProductService>(); 
-            builder.RegisterType<EfProductDAL>().As<IProductDAL>(); 
+            builder.RegisterType<ProductManager>().As<IProductService>();
+            builder.RegisterType<EfProductDAL>().As<IProductDAL>();
 
-            builder.RegisterType<UserManager>().As<IUserService>(); 
-            builder.RegisterType<EfUserDAL>().As<IUserDAL>(); 
-            
-            builder.RegisterType<AuthManager>().As<IAuthService>(); 
+            builder.RegisterType<UserManager>().As<IUserService>();
+            builder.RegisterType<EfUserDAL>().As<IUserDAL>();
+
+            builder.RegisterType<AuthManager>().As<IAuthService>();
             builder.RegisterType<TokenHelper>().As<ITokenHelper>();
+
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
+                .EnableInterfaceInterceptors(new Castle.DynamicProxy.ProxyGenerationOptions
+                {
+                    Selector = new AspectInterceptorSelector()
+                }).SingleInstance();
         }
     }
 }
---
+++ Core/Utility/Interceptors/MethodInterception.cs
@@ -0,0 +1,25 @@
+﻿using Castle.DynamicProxy;
+
+using System.Collections.Generic;
+
+namespace Core.Utility.Interceptors
+{
+    public class MethodInterception : MethodInterceptionBaseAttribute
+    {
+        protected virtual void OnBefor(IInvocation invocation)
+        {
+        }
+        protected virtual void OnAfter(IInvocation invocation) 
+        { 
+        }
+        protected virtual void OnException(IInvocation invocation) 
+        { 
+        }
+        protected virtual void OnSuccess(IInvocation invocation) 
+        { 
+        }
+        public override void Intercept(IInvocation invocation) 
+        { 
+        }
+    }
+}
